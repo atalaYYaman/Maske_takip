c380a482b624b2ab446b683780981f43d967f55e
Atalay Yaman <atalayyaman770@gmail.com>
2024-01-30 12:51:41 +0300

    first commit

---
+++ Workspace/Program.cs
@@ -0,0 +1,23 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace Workspace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.FirstName = "Atalay";
+            person1.LastName = "Yaman";
+            person1.DateOfBirthYear = 2004;
+            person1.NationalIdentity = 55693232498;
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+        }
+
+     
+    }
+}
